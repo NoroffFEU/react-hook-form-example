c977d8487cb026aa30e731c145aa2659da881791
Raymond Johannessen <rajohan1@gmail.com>
2021-05-07 13:27:20 +0200

    Update to use react-hook-form v7

--- src/App.js
+++ src/App.js
@@ -4,36 +4,40 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import "./App.css";
 
 const schema = yup.object().shape({
-	name: yup.string().required("Please enter your name"),
-	email: yup.string().required("Please enter an email address").email("Please enter a valid email address"),
-	message: yup.string().required("Please enter your message").min(10, "The message must be at least 10 characters"),
+    name: yup.string().required("Please enter your name"),
+    email: yup.string().required("Please enter an email address").email("Please enter a valid email address"),
+    message: yup.string().required("Please enter your message").min(10, "The message must be at least 10 characters")
 });
 
 function App() {
-	const { register, handleSubmit, errors } = useForm({
-		resolver: yupResolver(schema),
-	});
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm({
+        resolver: yupResolver(schema)
+    });
 
-	function onSubmit(data) {
-		console.log(data);
-	}
+    function onSubmit(data) {
+        console.log(data);
+    }
 
-	console.log(errors);
+    console.log(errors);
 
-	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<input name="name" ref={register} />
-			{errors.name && <span>{errors.name.message}</span>}
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register("name")} />
+            {errors.name && <span>{errors.name.message}</span>}
 
-			<input name="email" ref={register} />
-			{errors.email && <span>{errors.email.message}</span>}
+            <input {...register("email")} />
+            {errors.email && <span>{errors.email.message}</span>}
 
-			<textarea name="message" ref={register}></textarea>
-			{errors.message && <span>{errors.message.message}</span>}
+            <textarea {...register("message")} />
+            {errors.message && <span>{errors.message.message}</span>}
 
-			<button>Send</button>
-		</form>
-	);
+            <button>Send</button>
+        </form>
+    );
 }
 
 export default App;
--- src/components/SimpleExample.js
+++ src/components/SimpleExample.js
@@ -1,24 +1,28 @@
 import { useForm } from "react-hook-form";
 
 function SimpleExample() {
-	const { register, handleSubmit, errors } = useForm();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm();
 
-	function onSubmit(data) {
-		console.log(data);
-	}
+    function onSubmit(data) {
+        console.log(data);
+    }
 
-	console.log(errors);
+    console.log(errors);
 
-	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<input name="firstNmame" ref={register} />
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register("firstName")} />
 
-			<input name="lastName" ref={register({ required: true })} />
-			{errors.lastName && <span>This field is required</span>}
+            <input {...register("lastName", { required: true })} />
+            {errors.lastName && <span>This field is required</span>}
 
-			<button>Send</button>
-		</form>
-	);
+            <button>Send</button>
+        </form>
+    );
 }
 
 export default SimpleExample;
